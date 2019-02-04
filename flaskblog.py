8dab955042c6e448e9ad25136d98a6bc518a5954
Prathamesh <pprathamesh1594@gmail.com>
2019-02-04 15:47:59 +0530

    create static and template files

--- flaskblog.py
+++ flaskblog.py
@@ -1,2 +1,33 @@
-from flask import Flask
+from flask import Flask, render_template
 
+app = Flask(__name__)
+
+posts = [
+ {
+ 'name': 'Sergio Aguero',
+ 'title': 'Stiker',
+ 'content': 'This is my poor performance in UEFA',
+ 'date_posted': 'April 10 2019'
+ },
+ {
+ 'name': 'David Silva',
+ 'title': 'Winning is our Hobby',
+ 'content': 'A Midfielder in a making news with overconfidence with citys win',
+ 'date_posted': 'April 09 2019'
+ }
+]
+
+
+
+@app.route('/')
+@app.route('/home')
+def hello():
+    return render_template('home.html', posts=posts)
+
+@app.route('/about')
+def about():
+    return render_template('about.html', title='About')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
